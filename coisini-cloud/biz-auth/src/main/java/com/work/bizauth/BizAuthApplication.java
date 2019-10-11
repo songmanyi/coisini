f32565304d3e73ba3f982dad7751401cfca959bb
csrsm <cm245129248>
2019-10-11 10:40:13 +0800

    配置忽略文件

---
+++ coisini-cloud/biz-auth/src/main/java/com/work/bizauth/BizAuthApplication.java
@@ -0,0 +1,13 @@
+package com.work.bizauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BizAuthApplication.class, args);
+    }
+
+}
